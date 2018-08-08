bc4d56851d0f9c3b474a8616262b58a85979e940
Iker Loïc de Icaza Astiz <i.de-icaza-astiz@sussex.ac.uk>
2018-08-09 00:18:48 +0100

    Add support to have gitconfig details left out

--- setup.sh
+++ setup.sh
@@ -1,7 +1,37 @@
+#!/bin/bash
 #this script will setup git specifics using the configs in this repo
 
 files="gitconfig gitignore gitexcludes"
 date=`date +"%Y%m%d"`
+
+PATTERN="NAME\|EMAIL"
+FILE="gitconfig"
+if grep -q $PATTERN $FILE;
+then
+    # echo "Here are the Strings with the Pattern '$PATTERN':"
+    echo "You need to update your gitconfig file"
+    echo "Put your actual name and email instead of '$PATTERN'"
+    echo "Leave the #gitignore comments at the end!"
+    echo -e "$(grep $PATTERN $FILE)\n"
+    echo "Exiting..."
+    exit 1
+else
+    echo "The strings '$PATTERN' are not found in '$FILE'"
+    echo -e "Proceeding\n"
+fi
+
+IGNORE="#gitignore"
+if grep -q $IGNORE $FILE;
+then
+    echo "The ignore comments '$IGNORE' are in:"
+    echo "$(grep $IGNORE $FILE)"
+    echo -e "Proceeding\n"
+else
+    echo "Error: The Pattern '$IGNORE' was NOT Found in '$FILE'"
+    echo "Exiting..."
+    exit 1
+fi
+
 
 for file in $files; do
     if [ -h ~/.$file ]; then
@@ -15,9 +45,3 @@ for file in $files; do
     echo "symlinking $PWD/$file to ~/.$file"
     ln -s $PWD/$file ~/.$file
 done
-
-
-if [ -d ~/bin ]
-	then cp bin/* ~/bin
-else cp -r bin ~/bin
-fi
